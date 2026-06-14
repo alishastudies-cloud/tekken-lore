2d825bfa3ef3eac30675f70e367962407ed817a8
alishastudies-cloud <alisha.studies@gmail.com>
2026-06-14 19:08:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach(button => {
+    button.addEventListener("click", () => {
+
+        const panel = button.nextElementSibling;
+
+        if(panel.style.display === "block"){
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+});
